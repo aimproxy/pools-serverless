64593da57748722c04601158017f651986f51bb1
aimproxy <8200383@estg.ipp.pt>
2023-03-11 12:22:40 +0000

    Read token0, token1 and liquidity from uniswap v3

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
-import {Address, configureChains, createClient, readContract} from "@wagmi/core";
+import {Address, configureChains, createClient, readContracts} from "@wagmi/core";
+import zip from 'lodash.zip'
+
 import {chains} from "./chains";
 import {providers} from "./providers";
 import {uniswapV3ABI} from "./protocols/uniswap-v3";
@@ -10,20 +12,29 @@ app.use(express.json());
 
 const {provider} = configureChains(chains, providers)
 createClient({
+    autoConnect: true,
     provider,
 })
 
 app.get(`/`, async (req, res) => {
     const contract = "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8"
 
-    const liquidity = await readContract({
-        address: contract as Address,
-        abi: uniswapV3ABI,
-        functionName: 'liquidity',
+    const functions = ['token0', 'token1', 'liquidity']
+    const balances = await readContracts({
+        allowFailure: true,
+        contracts: functions.map((fn) => ({
+            address: contract as Address,
+            abi: uniswapV3ABI,
+            functionName: fn,
+        }) as const)
     })
 
+    const zipped = zip(functions, balances)
     return res.status(200).json({
-        [contract]: liquidity
+        [contract]: {
+            ...Object.fromEntries(zipped)
+        },
+        etherscan: `https://etherscan.io/address/${contract}`
     })
 });
 
