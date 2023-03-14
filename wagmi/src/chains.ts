7614c5380730a0f9a3e929ec4af50ce4d48e47a1
aimproxy <8200383@estg.ipp.pt>
2023-03-14 19:13:34 +0000

    Moving to ethers.js

--- wagmi/src/chains.ts
+++
@@ -1,6 +0,0 @@
-import type {Chain} from "@wagmi/core";
-import {mainnet} from "@wagmi/core/chains";
-
-export const chains: Chain[] = [
-    mainnet // eth chain
-]
--- wagmi/src/providers.ts
+++
@@ -1,8 +0,0 @@
-import type {ChainProviderFn} from "@wagmi/core";
-import {publicProvider} from "@wagmi/core/providers/public";
-
-export const providers: ChainProviderFn[] = [
-    publicProvider({
-        priority: 1,
-    }),
-]
