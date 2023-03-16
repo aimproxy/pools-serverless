b2aa6ad6a0c4dd23f3b5f5f7732a303275181554
aimproxy <8200383@estg.ipp.pt>
2023-03-16 10:21:00 +0000

    Gather info about token0 and token1

--- api/uniswap/v2/pool/[chain]/[contract].ts
+++ api/uniswap/v2/pool/[chain]/[contract].ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import { UniswapV2Pair__factory as IUniswapV2Pair } from '../../../../../types'
+import { ERC20__factory as ERC20, UniswapV2Pair__factory as UniswapV2Pair } from '../../../../../types'
 import { ethers } from 'ethers'
 
 const RPC_HOST = 'https://mainnet.infura.io/v3/d45cd5f5f19b4ff8b6d00265ecb56a6b'
@@ -11,18 +11,31 @@ export default async (req: VercelRequest, res: VercelResponse) => {
 
   const provider = new ethers.JsonRpcProvider(RPC_HOST)
 
-  const pair = IUniswapV2Pair.connect(contract, provider)
+  // Liquidity Pool Contract Consts
+  // Can be read from IUniswapV2ERC20 contract
+  //string public constant name = 'Uniswap V2';
+  //string public constant symbol = 'UNI-V2';
+  //uint8 public constant decimals = 18;
 
-  const name = await pair.name()
-
-  const [symbol, decimals] = await Promise.all([
-    pair.symbol(),
-    pair.decimals(),
-  ])
+  const pair = UniswapV2Pair.connect(contract, provider)
 
   const [token0, token1] = await Promise.all([
     pair.token0(),
     pair.token1(),
+  ])
+
+  const token0ERC20 = ERC20.connect(token0, provider)
+  const [balance0, symbol0, decimals0] = await Promise.all([
+    token0ERC20.balanceOf(contract), // contract is the lp contract addr
+    token0ERC20.symbol(),
+    token0ERC20.decimals(),
+  ])
+
+  const token1ERC20 = ERC20.connect(token1, provider)
+  const [balance1, symbol1, decimals1] = await Promise.all([
+    token1ERC20.balanceOf(contract),
+    token1ERC20.symbol(),
+    token1ERC20.decimals(),
   ])
 
   const reserves = await pair.getReserves()
@@ -31,14 +44,17 @@ export default async (req: VercelRequest, res: VercelResponse) => {
   res.status(200).json({
     chain,
     [contract]: {
-      pool: name,
-      lp_token: {
-        symbol,
-        decimals: decimals.toString(),
+      token: 'have it later',
+      pair: `${symbol0}/${symbol1}`,
+      token0: {
+        token: token0,
+        decimals: decimals0.toString(),
+        balance: balance0.toString(),
       },
-      pair: {
-        token0,
-        token1,
+      token1: {
+        token: token1,
+        decimals: decimals1.toString(),
+        balance: balance1.toString(),
       },
       kLast: kLast.toString(),
       //reserves,
