dd4102da1892af5120e0f4deeedf3dfe078130e5
aimproxy <8200383@estg.ipp.pt>
2023-03-17 10:43:40 +0000

    Document endpoints

--- api/endpoints.ts
+++ api/endpoints.ts
@@ -4,11 +4,10 @@ export default async (req: VercelRequest, res: VercelResponse) => {
   res.status(200).json({
     uniswap: {
       v2: {
-        params: {
-          0: 'lp owner contract',
-          1: 'lp contract',
+        pool: {
+          template: '/api/uniswap/v2/pool/{owner_contract}/{pool_contract}',
+          run: '/api/uniswap/v2/pool/0xab0b79f8357cba2c33a6d2815b85c7624959fea7/0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852',
         },
-        pool: '/api/uniswap/v2/pool/0xab0b79f8357cba2c33a6d2815b85c7624959fea7/0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852',
       },
     },
   })
